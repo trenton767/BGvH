868810273dcf1b795bd48d86d85d1ee7b4aa5f23
Travis Renton <travisrenton@ymail.com>
2025-02-06 10:49:28 -0500

    Working on Forms

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-import JSONSchemaForm from "./hooks/json_schema_form";
-
-let Hooks = {};
-Hooks.JSONSchemaForm = JSONSchemaForm;
-
-let liveSocket = new LiveSocket("/live", Socket, {
-  params: { _csrf_token: csrfToken },
-  hooks: Hooks // <- Add this
-});
